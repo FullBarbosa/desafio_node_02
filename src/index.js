6004356342b27dc83d243b72e10bedff452fd692
FullBarbosa <diibarbosa22@gmail.com>
2021-09-03 11:07:35 -0300

    mideware

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username} = request.headers;
+
+  const user = users.find(user =>  user.username === username); 
+  if(!user){
+    return response.status(404).json({ error:"not user checked"})
+  }
+
+  request.user = user;
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if(user.pro || user.todos <= 10){
+    return next();
+  }else{
+    return response.status(403).json({ error:"not user checked"})
+  }
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username} = request.headers;
+  const { id } = request.params;
+
+  const user = users.find( user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "Não existe usuário"});
+  }
+
+  const checkId=validate(id);
+  if(!checkId){
+    return response.status(400).json({error: "Não é um uuid valido"});
+  }
+
+  const todo = user.todos.find( todo => todo.id === id);
+  if(!todo){
+    return response.status(404).json({error: "Não pertence ao usuário"});
+  }
+
+
+  request.user= user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+  if(!user){
+    return response.status(404).json({ error:"not user checked"})
+  }
+
+
+  request.user=user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
